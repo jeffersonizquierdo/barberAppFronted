958ce6a21c9fddff659563899aa5c75ac77993fa
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-07-15 10:57:20 -0500

    home of barbershop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeBarbershopComponent } from './components/home-barbershop/home-barbershop.component';
+import { FormsModule } from '@angular/forms';
+
+
+const appRoutes: Routes = [
+
+  {path:'homebarbershop', component:HomeBarbershopComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeBarbershopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
