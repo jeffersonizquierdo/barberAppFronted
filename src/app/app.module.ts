26055fe4cfa0d0f17f35675952c48be7c518a369
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-07-15 13:44:12 -0500

    component ranking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeBarbershopComponent } from './components/home-barbershop/home-barbershop.component';
 import { FormsModule } from '@angular/forms';
+import { HomeCustomerComponent } from './components/home-customer/home-customer.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { HeaderBarbershopComponent } from './components/header-barbershop/header-barbershop.component';
 
 
 const appRoutes: Routes = [
 
-  {path:'homebarbershop', component:HomeBarbershopComponent}
+  
+  {path:'homebarbershop', component:HomeBarbershopComponent},
+  {path:'homecustomer', component:HomeCustomerComponent},
 
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeBarbershopComponent
+    HomeBarbershopComponent,
+    HomeCustomerComponent,
+    RankingComponent,
+    HeaderBarbershopComponent
   ],
   imports: [
     BrowserModule,
