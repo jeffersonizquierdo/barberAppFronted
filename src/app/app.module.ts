d29e224f24ab585b57498a131e71fa32690e0bbc
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-07-17 18:03:02 -0500

    appointmen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeBarbershopComponent } from './components/home-barbershop/home-barbershop.component';
@@ -9,7 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { HomeCustomerComponent } from './components/home-customer/home-customer.component';
 import { RankingComponent } from './components/ranking/ranking.component';
 import { HeaderBarbershopComponent } from './components/header-barbershop/header-barbershop.component';
-import { HeaderCustomerComponent } from './header-customer/header-customer.component';
+import { TableAppointmentComponent } from './components/table-appointment/table-appointment.component';
+import { HeaderCustomerComponent } from './components/header-customer/header-customer.component';
 
 
 const appRoutes: Routes = [
@@ -27,7 +27,8 @@ const appRoutes: Routes = [
     HomeCustomerComponent,
     RankingComponent,
     HeaderBarbershopComponent,
-    HeaderCustomerComponent
+    HeaderCustomerComponent,
+    TableAppointmentComponent
   ],
   imports: [
     BrowserModule,
