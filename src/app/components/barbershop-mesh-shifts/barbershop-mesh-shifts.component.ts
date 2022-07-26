a570e9ee12e19cf5ba3437486a5e8e83e5610465
Santiago21ruiz <ruiz08399@gmail.com>
2022-07-26 12:54:38 -0500

    segundo intento de creacion de mallas de turno

---
+++ src/app/components/barbershop-mesh-shifts/barbershop-mesh-shifts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barbershop-mesh-shifts',
+  templateUrl: './barbershop-mesh-shifts.component.html',
+  styleUrls: ['./barbershop-mesh-shifts.component.css']
+})
+export class BarbershopMeshShiftsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
