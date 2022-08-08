77419ce6ddc867790b7328c4f627edd2e4d78676
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-08-08 10:13:50 -0500

    validaciones en front

--- src/app/services/barber/barber.service.ts
+++ src/app/services/barber/barber.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Barber } from 'src/app/models/Barber';
+import swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,21 @@ export class BarberService {
 
   constructor(private http:HttpClient) { }
 
+  private httpHeadres = new HttpHeaders({'Content-Type' : 'application/json'})
 
-  saveBarber(newBarber: Barber): Observable<Barber>{
+  saveBarber(newBarber: Barber): Observable<any>{
 
-    return this.http.post<Barber>("http://localhost:8080/barber/save", newBarber)
+    return this.http.post<any>("http://localhost:8080/barber/save", newBarber, {headers: this.httpHeadres}).pipe(
+
+    
+
+      catchError(e =>{
+
+        console.error(e.error.Mensaje);
+        swal.fire(e.error.Mensaje, e.error.Error, 'error');
+        return throwError(e);
+      })
+    )
+    
   }
 }
