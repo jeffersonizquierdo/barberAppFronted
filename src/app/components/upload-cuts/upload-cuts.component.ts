58002af1e4bdfb14f8d84334b0f9daa0bd8de166
Andres Camilo <camiloser2004@gmail.com>
2022-08-19 10:09:57 -0500

    se agregaron los componentes upload-cut y card-loader

---
+++ src/app/components/upload-cuts/upload-cuts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-cuts',
+  templateUrl: './upload-cuts.component.html',
+  styleUrls: ['./upload-cuts.component.css']
+})
+export class UploadCutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
