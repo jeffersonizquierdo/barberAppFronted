48045051dcc9e401914a87198106d21ebe5ff19d
camilov04 <velasquezhurtadoc@gmail.com>
2022-08-24 17:45:45 -0500

    actualizacion componente publicar

--- src/app/services/publicity/publicity.service.ts
+++ src/app/services/publicity/publicity.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthServices } from 'src/app/models/AuthServices';
 import { Publicity } from 'src/app/models/Publicity';
 
 
@@ -9,15 +10,33 @@ import { Publicity } from 'src/app/models/Publicity';
 })
 export class PublicityService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService: AuthServices) { }
+
+  private httpHeadres = new HttpHeaders({ 'Content-Type' : 'application/json'})
 
   savePublicity(newPublicity:Publicity): Observable<Publicity>{
 
-    return this.httpClient.post<Publicity>("http://localhost:8080/publication/save", newPublicity)
+    return this.httpClient.post<Publicity>("http://localhost:8080/publication/save", newPublicity, {headers: this.agregarAuthorizationHeader()})
   }
 
+  
   listPublicity():  Observable<Publicity>{
 
-    return  this.httpClient.get<Publicity>("http://localhost:8080/publication/consultall")
+    return  this.httpClient.get<Publicity>("http://localhost:8080/publication/consultall", {headers: this.agregarAuthorizationHeader()} )
   }
+
+
+  private agregarAuthorizationHeader(){
+
+    let token = this.authService.token
+
+    if (token != null){
+      console.log(token + " tokennnnn");
+
+      return this.httpHeadres.append('Authorization', 'Bearer ' + token);
+    }
+
+    return this.httpHeadres
+
+  } 
 }
