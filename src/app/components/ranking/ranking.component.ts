761b6213cb96681f5e00e19703b028584cc3f467
Brayan Riascos <briascosarroyo@gamail.com>
2022-08-26 11:23:58 -0500

    se inicio el desarrollo del componente de ranking

--- src/app/components/ranking/ranking.component.ts
+++ src/app/components/ranking/ranking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarberService } from 'src/app/services/barber/barber.service';
 
 @Component({
   selector: 'app-ranking',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RankingComponent implements OnInit {
 
-  constructor() { }
+  listBarber:any=[];
+  rankingBarber:any=[];
+
+  constructor(private serveceBarber:BarberService) { }
+
+
 
   ngOnInit(): void {
+    this.organizeRanking();
+  }
+
+  organizeRanking():void{
+    this.serveceBarber.listBarber().subscribe(
+      data=>{
+        this.listBarber=data;
+        this.listBarber.sort((a,b)=>{
+          if(a.qualification>b.qualification){
+            return -1;
+          }
+          if(a.qualification<b.qualification){
+            return 1;
+          }
+          if(a.nickname.toLowerCase() > b.nickname.toLowerCase()){
+            return 1;
+          }
+          if(a.nickname.toLowerCase() < b.nickname.toLowerCase()){
+            return -1;
+          }
+          return 0;
+        })
+      })
+
+      for(let i=0; i<10; i++){
+        this.rankingBarber.push(this.listBarber[i]);
+      }
   }
 
 }
