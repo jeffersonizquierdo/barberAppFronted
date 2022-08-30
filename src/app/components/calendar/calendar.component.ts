7f02afab79c12ad71789c5284b98d91f448dc5e2
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-08-30 10:25:33 -0500

    agregando citas manualmente

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { View } from '@syncfusion/ej2-angular-schedule';
+import { View, EventSettingsModel } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-calendar',
   // templateUrl: './calendar.component.html',
-  template: '<ejs-schedule [currentView]="setView"></ejs-schedule>',
+  template: '<ejs-schedule  [eventSettings]="eventObject"  [currentView]="setView"></ejs-schedule>',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
@@ -14,6 +14,17 @@ export class CalendarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public setView : View = "Month"; 
+  public setView : View = "Week";
+  public setDate: Date =  new Date(2022, 7,15)
+  public eventObject: EventSettingsModel = {
+
+    dataSource: [{
+      
+      Subject: "Reserva",
+      StartTime: new Date (2022, 7, 30, 12, 0),
+      EndTime: new Date (2022, 7, 31, 16, 0),
+      IsReadonly: true
+    }]
+  }
 
 }
