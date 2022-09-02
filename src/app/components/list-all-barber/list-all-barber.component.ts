18e6bdcd93edfdf08422ad66ab4e0847ac269e9d
Brayan Riascos <briascosarroyo@gamail.com>
2022-09-01 23:02:04 -0500

    se inicio el desarrollo de vinculacion de barberos a la barberias

--- src/app/components/list-all-barber/list-all-barber.component.ts
+++ src/app/components/list-all-barber/list-all-barber.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalBarbershopComponent } from 'src/app/modals/modal-barbershop/modal-barbershop.component';
+import { AuthServices } from 'src/app/models/AuthServices';
+import { Barber } from 'src/app/models/Barber';
+import { Barbershop } from 'src/app/models/barbershop';
+import { Linkear } from 'src/app/models/linkear';
+import { Usuario } from 'src/app/models/Usuario';
 import { BarberService } from 'src/app/services/barber/barber.service';
+import { BarbershopService } from 'src/app/services/barbershop/barbershop.service';
+import { LinkearService } from 'src/app/services/linkear/linkear.service';
 
 @Component({
   selector: 'app-list-all-barber',
@@ -9,9 +18,15 @@ import { BarberService } from 'src/app/services/barber/barber.service';
 export class ListAllBarberComponent implements OnInit {
 
   barbers:any=[];
-  constructor(private BarberService:BarberService) { }
+  constructor(private BarberService:BarberService, private auhtService: AuthServices, private serviceBarbershop:BarbershopService,private modalService:NgbModal,private serveceLinkear:LinkearService) { }
+  usuario:Usuario;
+  usuarioConsult:Usuario;
+  barbershop:Barbershop;
+  bonding:Linkear;
+  barbero:Barber;
 
   ngOnInit(): void {
+    this.usuario=this.auhtService.usuario;
     this.loaderBarber();
   }
 
@@ -20,9 +35,34 @@ export class ListAllBarberComponent implements OnInit {
       data =>{
         this.barbers = data;
         console.log(this.barbers);
-        
       }
     )
   }
+
+  linkear(id_barber:Number){
+    this.barbershop=new Barbershop();
+    this.serviceBarbershop.getbarber(this.usuario.id).subscribe((response:any)=>{
+      this.barbershop=response;
+      if(this.barbershop==null){
+        this.abrirModal();
+      }else{
+        this.bonding= new Linkear();
+        this.bonding.barbershop=this.barbershop
+        this.bonding.acceptance=false;
+        this.barbero=new Barber();
+        this.BarberService.getbarber(id_barber).subscribe((response:any)=>{
+          this.barbero=response;
+          this.bonding.barber=this.barbero;
+        })
+      }
+    })
+    this.serveceLinkear.saveLinkear(this.bonding).subscribe(response=>{
+      console.log(response);
+    })
+  }
+
+  abrirModal(){
+    this.modalService.open(ModalBarbershopComponent);
+}
 
 }
---
+++ src/app/services/linkear/linkear.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AuthServices } from 'src/app/models/AuthServices';
+import { Linkear } from 'src/app/models/linkear';
+import { Usuario } from 'src/app/models/Usuario';
+import swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinkearService {
+
+ 
+
+  constructor(private http:HttpClient, private authService: AuthServices) { }
+
+  private httpHeadres = new HttpHeaders({'Content-Type' : 'application/json'})
+
+  usuario:Usuario = this.authService.usuario;
+
+  private agregarAuthorizationHeader(){
+
+    let token = this.authService.token
+
+    if (token != null){
+      console.log(token + " tokennnnn");
+
+      return this.httpHeadres.append('Authorization', 'Bearer ' + token);
+    }
+
+    return this.httpHeadres
+
+  }
+
+  saveLinkear(newLinkear: Linkear): Observable<any>{
+
+    return this.http.post<any>("http://localhost:8080/barber/save", newLinkear, {headers: this.agregarAuthorizationHeader()}).pipe(
+
+    catchError(e =>{
+
+      console.error(e.error.Mensaje);
+      swal.fire(e.error.Mensaje, e.error.Error, 'error');
+      return throwError(e);
+    })
+    )
+    
+  }
+}
